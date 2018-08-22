ff24ff065b05ce0564d2a3b759a094b48f36dab9
Nick Koutrelakos <wkoutre@gmail.com>
2018-08-22 12:51:26 -0700

    fixed fontFamily PR

--- example/ExampleTextStyles/ExampleTextStyles.js
+++ example/ExampleTextStyles/ExampleTextStyles.js
@@ -61,31 +61,6 @@ const ExampleTextStyles = StyleSheet.create({
     fontWeight: DEFAULT_P_FONT_WEIGHT,
     fontFamily: FONT_FAMILY
   })
-
-  // h1Button: {
-  //   fontFamily: FONT_FAMILY,
-  //   fontSize: 24,
-  //   letterSpacing: 3.8
-  // },
-  // h2Button: {
-  //   fontFamily: FONT_FAMILY,
-  //   fontSize: 18,
-  //   letterSpacing: 2.9
-  // },
-  // h3Button: {
-  //   fontFamily: FONT_FAMILY,
-  //   fontSize: 16,
-  //   letterSpacing: 1.7
-  // },
-  // h4Button: {
-  //   fontFamily: FONT_FAMILY,
-  //   fontSize: 14,
-  //   letterSpacing: 0.25
-  // },
-  // h5Button: {
-  //   fontFamily: FONT_FAMILY,
-  //   fontSize: 12
-  // }
 });
 
 export { ExampleTextStyles };
